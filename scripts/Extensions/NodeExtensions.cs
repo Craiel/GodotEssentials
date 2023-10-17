d930032472f05c8a8f1e4f311a3ff486b5a45bb1
Patrick Gustavel <craieldev@gmail.com>
2023-10-17 00:44:04 -0400

    node helpers for instantiation

--- scripts/Extensions/NodeExtensions.cs
+++ scripts/Extensions/NodeExtensions.cs
@@ -1,6 +1,7 @@
 namespace Craiel.Essentials.Extensions;
 
 using System;
+using System.IO;
 using System.Reflection;
 using Attributes;
 using Godot;
@@ -8,6 +9,35 @@ using Utils;
 
 public static class NodeExtensions
 {
+	public static T Instantiate<T>(this PackedScene prefab)
+		where T: class
+	{
+		if (prefab == null)
+		{
+			throw new InvalidOperationException("Instantiate called with null prefab");
+		}
+		
+		var instance = prefab.Instantiate();
+		if (instance == null)
+		{
+			throw new InvalidOperationException("Instantiation failed");
+		}
+		
+		var result = instance.GetRootNode<T>();
+		if (result == null)
+		{
+			throw new InvalidDataException("Prefab " + prefab.ResourceName + " has no root node of type: " + typeof(T));
+		}
+
+		return result;
+	}
+	
+	public static T GetRootNode<T>(this Node scene)
+		where T: class
+	{
+		return scene.GetNode<T>(".");
+	}
+	
 	public static void DeserializeNodeMeta(this Node node)
 	{
 		var nodeType = node.GetType();
@@ -25,6 +55,12 @@ public static class NodeExtensions
 			if (field.FieldType == TypeDef<double>.Value)
 			{
 				field.SetValue(node, meta.AsDouble());
+				continue;
+			}
+
+			if (field.FieldType == TypeDef<Color>.Value)
+			{
+				field.SetValue(node, meta.AsColor());
 				continue;
 			}
 
