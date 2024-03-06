774f0155577ca9b862bec74ed006999c529e666c
Patrick Gustavel <craieldev@gmail.com>
2024-03-06 11:07:51 -0500

    indentation fixes

--- scripts/Logging/GodotLogRelay.cs
+++ scripts/Logging/GodotLogRelay.cs
@@ -7,30 +7,30 @@ using Godot;
 
 public class GodotLogRelay
 {
-    public void Info(string message)
-    {
-        GD.Print(FormatMessage(message));
-    }
-    
-    public void Warn(string message)
-    {
-        GD.PushWarning(FormatMessage(message));
-    }
+	public void Info(string message)
+	{
+		GD.Print(FormatMessage(message));
+	}
+	
+	public void Warn(string message)
+	{
+		GD.PushWarning(FormatMessage(message));
+	}
 
-    public void Error(string message)
-    {
-        GD.PushError(FormatMessage(message));
-    }
-    
-    public void Error<T>(string message, T exception = null)
-        where T: Exception
-    {
-        GD.PushError(FormatMessage(message));
-    }
+	public void Error(string message)
+	{
+		GD.PushError(FormatMessage(message));
+	}
+	
+	public void Error<T>(string message, T exception = null)
+		where T: Exception
+	{
+		GD.PushError(FormatMessage(message));
+	}
 
-    static string FormatMessage(string message)
-    {
-        TimeSpan time = TimeSpan.FromMilliseconds(Time.GetTicksMsec());
-        return $"[{Thread.CurrentThread.ManagedThreadId}] {time:g}: {message}";
-    }
+	static string FormatMessage(string message)
+	{
+		TimeSpan time = TimeSpan.FromMilliseconds(Time.GetTicksMsec());
+		return $"[{Thread.CurrentThread.ManagedThreadId}] {time:g}: {message}";
+	}
 }
