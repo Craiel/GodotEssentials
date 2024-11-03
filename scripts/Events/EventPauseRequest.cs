1a7404ad4bed36c6c17c710f98525795bf9a9b9f
Craieldev <craieldev@gmail.com>
2024-11-03 16:30:48 -0500

    Helper node for pausing game

---
+++ scripts/Events/EventPauseRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Craiel.Essentials.Events;
+
+using Craiel.Essentials.Contracts;
+
+public class EventPauseRequest : IGameEvent
+{
+    public EventPauseRequest(bool pause)
+    {
+        this.RequestedState = pause;
+    }
+
+    public bool RequestedState;
+}
---
+++ scripts/Nodes/PauseHandlerNode.cs
@@ -0,0 +1,37 @@
+﻿namespace Craiel.Essentials.Nodes;
+
+using Event;
+using Events;
+using Godot;
+
+public partial class PauseHandlerNode : Node
+{
+    private BaseEventSubscriptionTicket pauseRequestEventTicket;
+    
+    // -------------------------------------------------------------------
+    // Public
+    // -------------------------------------------------------------------
+    [Export] public Node Target;
+
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+        
+        GameEvents.Subscribe<EventPauseRequest>(this.OnPauseRequest, out this.pauseRequestEventTicket);
+    }
+
+    public override void _ExitTree()
+    {
+        GameEvents.Unsubscribe(ref this.pauseRequestEventTicket);
+        
+        base._ExitTree();
+    }
+
+    // -------------------------------------------------------------------
+    // Private
+    // -------------------------------------------------------------------
+    private void OnPauseRequest(EventPauseRequest eventData)
+    {
+        this.Target.GetTree().Paused = eventData.RequestedState;
+    }
+}
