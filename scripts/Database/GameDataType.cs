33f94eb2ec11fc0c2280f60e53c3435b7999f392
Craieldev <craieldev@gmail.com>
2024-11-23 22:58:05 -0500

    Adding data types

--- scripts/Database/GameDataType.cs
+++ scripts/Database/GameDataType.cs
@@ -4,8 +4,18 @@ public enum GameDataType
 {
     Unset = 0,
     
+    // NOTE: Do not modify, only add new types
     Character = 1,
     Zone = 2,
     Dialog = 3,
-    Items = 4
+    Item = 4,
+    Spell = 5,
+    Monster = 6,
+    Quest = 7,
+    Affix = 8,
+    Class = 9,
+    Currency = 10,
+    LootTable = 11,
+    Rarity = 12,
+    Stat = 13
 }
