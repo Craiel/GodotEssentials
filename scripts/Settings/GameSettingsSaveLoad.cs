5ffb9cebe650bd0efc827133488ee2028fea3b40
Craieldev <craieldev@gmail.com>
2025-11-27 17:47:07 -0500

    Removing slot from default save / load settings

--- scripts/Settings/GameSettingsSaveLoad.cs
+++ scripts/Settings/GameSettingsSaveLoad.cs
@@ -2,8 +2,6 @@ namespace Craiel.Essentials.Settings;
 
 public static class GameSettingsSaveLoad
 {
-    private const string LastSaveSlotKey = "last_save_slot";
-    
     // -------------------------------------------------------------------
     // Public
     // -------------------------------------------------------------------
@@ -13,31 +11,5 @@ public static class GameSettingsSaveLoad
 
     public static void Apply()
     {
-    }
-    
-    public static void SetLastSaveSlot(int newValue, bool save = true, bool apply = true)
-    {
-        int currentValue = GameSettings.Get(GameSettingsSection.General, LastSaveSlotKey, -1).AsInt32();
-        if (currentValue == newValue)
-        {
-            return;
-        }
-        
-        GameSettings.Set(GameSettingsSection.General, LastSaveSlotKey, newValue);
-
-        if (save)
-        {
-            GameSettings.Save();
-        }
-
-        if (apply)
-        {
-            Apply();
-        }
-    }
-
-    public static int GetLastSaveSlot()
-    {
-        return GameSettings.Get(GameSettingsSection.General, LastSaveSlotKey, -1).AsInt32();
     }
 }
